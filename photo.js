6d63111802c96af1dccdbcd1db112889e807ee24
Olivia Bizimungu <oibizimungu@mta.ca>
2022-05-24 08:13:36 -0400

    Link update

--- photo.js
+++ photo.js
@@ -5,6 +5,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if (val.match(/\.(jpe?g|png|gif|JPG)$/)) {
+                val = '.' + val
                 set_image(val)
             }
         });
